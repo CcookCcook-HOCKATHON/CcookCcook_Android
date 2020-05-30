e81231790a952e30d1b469a689713669f94e321c
Hyun Woo Lee <l2hyunwoo@konkuk.ac.kr>
2020-05-31 05:40:03 +0900

    그리드 레이아웃 + 메인 배경색 바꿈

--- app/src/main/java/com/example/ccookccook/HomeFragment.kt
+++ app/src/main/java/com/example/ccookccook/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -20,6 +21,8 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        rv_recipe.layoutManager = GridLayoutManager(this.context, 2)
+        rv_recipe.addItemDecoration(RecipeItemDecoration(14))
         var recipeAdapter = RecipeAdapter(view.context)
         recipeAdapter.datas = RecipeDummy().recipeList() as MutableList<RecipeData>
         recipeAdapter.notifyDataSetChanged()
